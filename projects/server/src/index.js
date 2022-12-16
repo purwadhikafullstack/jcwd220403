ab13d124ac725c053d829e7a6d716cd339ad09f1
Agung Rizka <mr.gomu@gmail.com>
2022-12-16 15:11:29 +0700

    feat: init project server

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -1,33 +1,37 @@
-require("dotenv/config");
-const express = require("express");
-const cors = require("cors");
-const { join } = require("path");
+require('dotenv/config');
+const express = require('express');
+const cors = require('cors');
+const bearerToken = require('express-bearer-token');
+const { join } = require('path');
+const database = require('../models');
+const { userRouters } = require('../routers');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.SERVER_PORT || 8000;
 const app = express();
 app.use(
   cors({
     origin: [
       process.env.WHITELISTED_DOMAIN &&
-        process.env.WHITELISTED_DOMAIN.split(","),
+        process.env.WHITELISTED_DOMAIN.split(','),
     ],
   })
 );
 
 app.use(express.json());
-
+app.use(bearerToken());
+app.use(userRouters);
 //#region API ROUTES
 
 // ===========================
 // NOTE : Add your routes here
 
-app.get("/api", (req, res) => {
+app.get('/api', (req, res) => {
   res.send(`Hello, this is my API`);
 });
 
-app.get("/api/greetings", (req, res, next) => {
+app.get('/api/greetings', (req, res, next) => {
   res.status(200).json({
-    message: "Hello, Student !",
+    message: 'Hello, Student !',
   });
 });
 
@@ -35,8 +39,8 @@ app.get("/api/greetings", (req, res, next) => {
 
 // not found
 app.use((req, res, next) => {
-  if (req.path.includes("/api/")) {
-    res.status(404).send("Not found !");
+  if (req.path.includes('/api/')) {
+    res.status(404).send('Not found !');
   } else {
     next();
   }
@@ -44,9 +48,9 @@ app.use((req, res, next) => {
 
 // error
 app.use((err, req, res, next) => {
-  if (req.path.includes("/api/")) {
-    console.error("Error : ", err.stack);
-    res.status(500).send("Error !");
+  if (req.path.includes('/api/')) {
+    console.error('Error : ', err.stack);
+    res.status(500).send('Error !');
   } else {
     next();
   }
@@ -55,12 +59,12 @@ app.use((err, req, res, next) => {
 //#endregion
 
 //#region CLIENT
-const clientPath = "../../client/build";
+const clientPath = '../../client/build';
 app.use(express.static(join(__dirname, clientPath)));
 
 // Serve the HTML page
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, clientPath, "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, clientPath, 'index.html'));
 });
 
 //#endregion
@@ -72,3 +76,7 @@ app.listen(PORT, (err) => {
     console.log(`APP RUNNING at ${PORT} ✅`);
   }
 });
+
+const user = database.user;
+
+// user.sync({ alter: true });
