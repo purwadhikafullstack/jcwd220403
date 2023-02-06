950c2a4bc88a71365b56ac09ac55fc6e660eb48c
Agung Rizka <mr.gomu@gmail.com>
2023-02-06 15:06:04 +0800

    paymentMethod to paymentmethod

--- projects/client/src/Components/Payment/PaymentInstruction/Timer.jsx
+++ projects/client/src/Components/Payment/PaymentInstruction/Timer.jsx
@@ -37,13 +37,13 @@ function Timer({ payment }) {
 
   const timerComponents = [];
 
-  Object.keys(timeLeft).forEach((interval) => {
+  Object.keys(timeLeft).forEach((interval, index) => {
     if (!timeLeft[interval]) {
       return;
     }
 
     timerComponents.push(
-      <span>
+      <span key={index}>
         {timeLeft[interval]} {interval}{' '}
       </span>
     );
--- projects/server/src/controllers/transaction/privateTransactionControllers.js
+++ projects/server/src/controllers/transaction/privateTransactionControllers.js
@@ -59,7 +59,7 @@ const getuserTransaction = async (req, res) => {
     const { userId } = req.params;
 
     const transactions = await sequelize.query(
-      `select t.id, t.transactionStatus, t.checkIn, t.CheckOut, t.userId, r.propertyId, pr.name as property_name, u.fullName, t.roomId, r.name as room_name, r.picture, p.paymentMethodId, p.total, rev.review 
+      `select t.id, t.transactionStatus, t.checkIn, t.CheckOut, t.userId, r.propertyId, pr.name as property_name, u.fullName, t.roomId, r.name as room_name, r.picture, p.paymentmethodId, p.total, rev.review 
       from transactions as t inner join payments as p on t.id = p.transactionId inner join 
       rooms as r on t.roomId = r.id 
       inner join properties as pr on pr.id = r.propertyId inner join 
